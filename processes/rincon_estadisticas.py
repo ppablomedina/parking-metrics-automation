143f7c5c0c52e8c53101ff519e16e0fd8796efc4
Pablo Medina <113461293+ppablomedina@users.noreply.github.com>
2025-07-01 14:44:33 +0100

    Update and rename processes_rincon_estadisticas.py to rincon_estadisticas.py

--- processes/rincon_estadisticas.py
+++ processes/rincon_estadisticas.py
@@ -1,15 +1,12 @@
 from current_month import w_month, year
-from gcp_paths import path_rincon_estadisticas as ss
-from gcp_utils import read
+from gcp.paths import path_rincon_estadisticas as ss
+from gcp.utils import read
 
 
 def main():
-
     sheet = read(ss, 'excel_sheet', f'{w_month.upper()} {str(year)[2:]}')
     
-    # Búsqueda del índice de la fila donde la primera columna sea 'Totales'
     totales_row = sheet[sheet.iloc[:, 0] == 'Totales']
-
     value = totales_row['Bono2'].values[0]
 
     return [
