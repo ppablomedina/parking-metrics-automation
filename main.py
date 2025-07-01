ca692202ac32157006dd344090c5ac6fc92e54e6
Pablo Medina <113461293+ppablomedina@users.noreply.github.com>
2025-07-01 14:33:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import router, processes_ocupacion, processes_abonados_en_banco, processes_abonados_lpa_y_qr, processes_recaudacion, processes_rincon_estadisticas, processes_abonados_y_rotacion, processes_informes_filtrados, processes_ratios
-from gcp_utils import insert_events
+from processes import router, ocupacion, abonados_en_banco, abonados_lpa_y_qr, recaudacion, rincon_estadisticas, abonados_y_rotacion, informes_filtrados, ratios
+from gcp.utils import insert_events
 
 def entry_point(request):
 
